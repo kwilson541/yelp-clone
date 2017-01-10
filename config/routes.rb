a05b85257305eaff681ec01f117e31b277733e73
unicornelia <ez.kornelia@gmail.com>
2017-01-10 15:25:14 +0000

    Starting with Devise, setting up User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: "restaurants#index"
 
   resources :restaurants do
     resources :reviews
   end
-  
+
 end
